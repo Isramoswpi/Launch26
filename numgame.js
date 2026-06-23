67f776dc5dfce4e269d163334e9617f8c6407c1b
Nitish <nbhatnagar@wpi.edu>
2026-06-23 12:23:29 -0400

    Day 2 Launch Website

---
+++ numgame.js
@@ -0,0 +1,77 @@
+//create variables to referance html documents
+const numField = document.getElementById("num-field");
+const guessButton = document.getElementById("guess-button");
+const resetButton = document.getElementById("reset-button");
+const messageText = document.getElementById("message-text");
+const guessCountText = document.getElementById("guess-count-text");
+
+//create min max variables
+let min = 1;
+let max = 100;
+
+//create a variable to hold the random number
+let secret;
+let guestCount = 0;
+
+let myConfetti = null; 
+if (window.confetti) {
+    myConfetti = confetti.create(null, {
+        resize: true,
+        useWorker: true
+    });
+}
+
+function loadGame() {
+    numField.min = min;
+    numField.max = max;
+    numField.value = "";
+    secret = Math.floor(Math.random() * (max - min + 1)) + min;
+    guessCount = 0;
+    messageText.textContent= `Enter a number between ${min} and ${max}.`;
+    guessCountText.innerHTML = "Guesses: 0";
+    numField.focus();
+
+}
+
+
+
+
+function makeGuess() {
+    const guess = parseInt(numField.value);
+    if (Number.isNaN(guess) || guess < min || guess > max) {
+        messageText.textContent.innterHTML= `Please enter a valid number between ${min} and ${max}.`;
+        return; 
+    }
+
+
+
+guessCount++;
+guessCountText.innerHTML = `Guesses: ${guessCount}`;
+
+if (guess === secret) {
+    messageText.innerHTML = `Congratulations! ${secret} is the correct number.`;
+    if (myConfetti) myConfetti({
+    particleCount: 3000, 
+     spread: 160
+
+    });
+     
+} else if (guess < secret) {
+    messageText.innerHTML = `${guess} is too low! Try again.`;
+} else {
+    messageText.innerHTML = `${guess} is too High! Try again.`;
+}
+}
+
+guessButton.addEventListener("click", makeGuess);
+resetButton.addEventListener("click", loadGame);
+
+numField.addEventListener("keydown", function(event){
+    if (event.key === "Enter") {
+        makeGuess(); 
+    }
+});
+
+
+//load the gme when the page loads
+loadGame();
